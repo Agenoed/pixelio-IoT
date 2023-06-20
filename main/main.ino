d87ff1aee6fcfb286361553e14e0e90e2ba55a2a
Igasus <ihor.hubskyi@gmail.com>
2023-06-20 16:44:21 +0300

    Added led matrix. (#1)
    Added led matrix initialization;
    Added matrix set on mqtt request receive.

--- main/main.ino
+++ main/main.ino
@@ -1,9 +1,13 @@
 #define HTTP_PORT 80
+#define LED_DIN_PIN 7
+#define NUM_LEDS_X 16
+#define NUM_LEDS_Y 16
 
 
 #include <Ethernet.h>
 #include <SPI.h>
 #include <ArduinoMqttClient.h>
+#include <FastLED.h>
 
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
@@ -17,11 +21,17 @@ const char mqttTopic[] = "pixelio/12345";
 EthernetClient ethernetClient;
 MqttClient mqttClient(ethernetClient);
 
+CRGB leds[NUM_LEDS_X * NUM_LEDS_Y];
+byte matrixBuffer[NUM_LEDS_X * NUM_LEDS_Y * 3];
+
 
 void setup();
 void loop();
 boolean init_ethernet();
 void onMqttMessage(int messageSize);
+void init_leds();
+void show_matrix();
+void set_pixel(int x, int y, CRGB color);
 
 
 void setup()
@@ -32,6 +42,19 @@ void setup()
     delay(1);
   }
   Serial.println("Serial initialized successfully.");
+
+  Serial.println("Initializing matrix...");
+  init_leds();
+  for (int y = 0; y < NUM_LEDS_Y; y++)
+  {
+    for (int x = 0; x < NUM_LEDS_X; x++)
+    {
+      int matrixBufferLedIndex = y * NUM_LEDS_X + x;
+      matrixBuffer[matrixBufferLedIndex] = 0;
+      matrixBuffer[matrixBufferLedIndex + 1] = 0;
+      matrixBuffer[matrixBufferLedIndex + 2] = 0;
+    }
+  }
 
   if (!init_ethernet())
   {
@@ -118,15 +141,75 @@ boolean init_ethernet()
 
 void onMqttMessage(int messageSize)
 {
-  Serial.println("Received a message with topic \"");
+  Serial.print("Received a message with topic \"");
   Serial.print(mqttClient.messageTopic());
   Serial.print("\", length: \"");
   Serial.print(messageSize);
-  Serial.println("\", bytes:");
-
-  while (mqttClient.available()) {
-    Serial.print((char)mqttClient.read());
+  Serial.println("\".");
+  
+  if (messageSize != NUM_LEDS_X * NUM_LEDS_Y * 3)
+  {
+    Serial.print("Message size is not equal to required. ");
+    Serial.println("Message processing canceled.");
+    return;
   }
+
+  Serial.println("Processing Message payload...");
+  for (int matrixBufferLedIndex = 0;
+    mqttClient.available() && matrixBufferLedIndex < NUM_LEDS_X * NUM_LEDS_Y * 3;
+    matrixBufferLedIndex++)
+  {
+    matrixBuffer[matrixBufferLedIndex] = (byte)mqttClient.read();
+  }
+  Serial.println("Message processed successfully. Printing Matrix.");
+  show_matrix();
   Serial.println();
-  Serial.println();
+}
+
+
+void init_leds()
+{
+  FastLED.addLeds<WS2812, LED_DIN_PIN, GRB>(leds, NUM_LEDS_X * NUM_LEDS_Y);
+
+  for (int ledIndex = 0; ledIndex < NUM_LEDS_X * NUM_LEDS_Y; ledIndex++)
+  {
+    leds[ledIndex] = CRGB(0, 0, 0);
+  }
+  FastLED.show();
+}
+
+
+void show_matrix()
+{
+  for (int y = 0; y < NUM_LEDS_Y; y++)
+  {
+    for (int x = 0; x < NUM_LEDS_X; x++)
+    {
+      int matrixBufferLedIndex = (y * NUM_LEDS_X + x) * 3;
+      byte colorR = matrixBuffer[matrixBufferLedIndex];
+      byte colorG = matrixBuffer[matrixBufferLedIndex + 1];
+      byte colorB = matrixBuffer[matrixBufferLedIndex + 2];
+      CRGB color = CRGB(colorR, colorG, colorB);
+      set_pixel(x, y, color);
+    }
+  }
+
+  FastLED.show();
+  Serial.println("Matrix printing finished.");
+}
+
+
+void set_pixel(int x, int y, CRGB color)
+{
+  int ledIndex;
+  if ((NUM_LEDS_Y - y) % 2 == 0)
+  {
+    ledIndex = (NUM_LEDS_Y - y) * NUM_LEDS_X - 1 - x;
+  }
+  else
+  {
+    ledIndex = (NUM_LEDS_Y - y - 1) * NUM_LEDS_X + x;
+  }
+
+  leds[ledIndex] = color;
 }
